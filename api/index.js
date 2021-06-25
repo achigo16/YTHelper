612b504bfa1f9d81191cc8c74f5e789f8742af99
Adityawan Chandra <adityawancha@gmail.com>
2021-06-25 11:52:03 +0700

    Update structur file & Add Support vercel

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
+
 const app = express();
 const PORT = 4000;
 
@@ -10,7 +11,14 @@ app.listen(PORT, () => {
   console.log(`running at port ${PORT}`);
 });
 
-app.get('/', async (req, res) => {
+app.get('/api', (req, res) => {
+  // Caching for vercel
+  res.setHeader('Content-Type', 'text/html')
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+})
+
+app.get('/api/:api', async (req, res) => {
+  const api = req.params?.api ?? false;
   const params = req.query?.params ?? false;
 
   try {
@@ -18,7 +26,7 @@ app.get('/', async (req, res) => {
       return res.sendStatus(400);
     }
 
-    const { url, api, formats, options = {} } = JSON.parse(params);
+    const { url, formats, options = {} } = JSON.parse(params);
 
     if (!api || !ytdl[api]) {
       return res.sendStatus(400);
@@ -28,11 +36,13 @@ app.get('/', async (req, res) => {
       return res.sendStatus(404);
     }
 
-    const respon = await ytdl[api](url || formats, options);
+    const response = await ytdl[api](url || formats, options);
 
-    res.send(respon);
-    res.sendStatus(200);
+    res.json(response);
   } catch (error) {
-    res.send('Err: ' + error);
+    res.sendStatus(500);
+    res.json({ error });
   }
 });
+
+module.exports = app
